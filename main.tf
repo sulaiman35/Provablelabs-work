7ba18c34575e2921c6abaaec4e0a530db84a66b5
Muhammad Sulaiman <sulaiman@Muhammads-MacBook-Pro-2.local>
2025-08-10 19:44:26 +0200

    Adding first file

--- main.tf
+++ main.tf
@@ -20,16 +20,6 @@ provider "aws" {
 }
 
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "state_bucket_encryption" {
-  bucket = aws_s3_bucket.terraform_state_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
 # ---
 # DynamoDB Table for State Locking
 # ---
